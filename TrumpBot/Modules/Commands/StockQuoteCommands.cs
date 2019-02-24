5bc5d97a00c47a0d598520cb627f973f70bb9a57
BadAtIrcBots <badatircbots@arstotzka.su>
2019-02-24 16:11:06 +0800

    Afterhours support

--- TrumpBot/Modules/Commands/StockQuoteCommands.cs
+++ TrumpBot/Modules/Commands/StockQuoteCommands.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text.RegularExpressions;
 using Meebey.SmartIrc4net;
 using TrumpBot.Models;
@@ -47,6 +48,45 @@ namespace TrumpBot.Modules.Commands
                 $"{b}52 Week Low/High{n}: {ticker.Week52Low:N}/{ticker.Week52High:N}";
         }
 
+        internal static string FormatAfterHoursTicker(IexApiModels.IexQuoteApiModel ticker)
+        {
+            var b = IrcConstants.IrcBold;
+            var n = IrcConstants.IrcNormal;
+            var c = IrcConstants.IrcColor;
+            var colours = new Colours();
+            var changeColour = colours.Green;
+            if (ticker.ExtendedChange < 0)
+            {
+                changeColour = colours.LightRed;
+            }
+
+            var ytdChangeColour = colours.Green;
+            if (ticker.YtdChange < 0)
+            {
+                ytdChangeColour = colours.LightRed;
+            }
+
+            var peRatioColour = colours.Green;
+            if (ticker.PeRatio < 0)
+            {
+                peRatioColour = colours.LightRed;
+            }
+
+            return
+                $"{b}{ticker.Symbol}{n} ({ticker.CompanyName}) - " +
+                $"{b}Latest (AH){n}: {ticker.ExtendedPrice:N} - " +
+                $"{b}Open{n}: {ticker.Open:N} - " +
+                $"{b}Close{n}: {ticker.Close:N} - " +
+                $"{b}High{n}: {ticker.High:N} - " +
+                $"{b}Low{n}: {ticker.Low:N} - " +
+                $"{b}Change (since close){n}: {c}{changeColour}{ticker.ExtendedChange:N}{n} ({c}{changeColour}{ticker.ExtendedChangePercent:P}{n}) - " +
+                $"{b}Volume{n}: {ticker.LatestVolume:N0} - " +
+                $"{b}Market Cap{n}: ${ticker.MarketCap:N0} - " +
+                $"{b}PE Ratio{n}: {c}{peRatioColour}{ticker.PeRatio}{n} - " +
+                $"{b}YTD Change{n}: {c}{ytdChangeColour}{ticker.YtdChange:P}{n} - " +
+                $"{b}52 Week Low/High{n}: {ticker.Week52Low:N}/{ticker.Week52High:N}";
+        }
+
         internal class GetLatestDailyQuoteBySymbol : ICommand
         {
             public string CommandName { get; } = "Get Stock Quote";
@@ -85,7 +125,16 @@ namespace TrumpBot.Modules.Commands
                 {
                     return new List<string>{"JSON is all fucked up."};
                 }
-                return new List<string>{FormatTicker(ticker)};
+
+                DateTimeOffset now = new DateTimeOffset(TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow,
+                    TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")), TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").BaseUtcOffset);
+                DateTimeOffset openTime = new DateTimeOffset(now.Year, now.Month, now.Day, 9, 30, 0, TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").BaseUtcOffset); // Even though it says standard time it is actually DST aware
+                DateTimeOffset closeTime = new DateTimeOffset(now.Year, now.Month, now.Day, 16, 0, 0, TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").BaseUtcOffset);
+                if ((now >= openTime && now <= closeTime) && !(now.DayOfWeek == DayOfWeek.Saturday || now.DayOfWeek == DayOfWeek.Sunday))
+                {
+                    return new List<string>{FormatTicker(ticker)};
+                }
+                return new List<string>{FormatAfterHoursTicker(ticker)};
             }
         }
 
