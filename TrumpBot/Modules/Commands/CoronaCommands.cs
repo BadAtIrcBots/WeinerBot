7591475dad6e0fe51c5a5f50608c10fd9c3e73d3
BadAtIrcBots <badatircbots@arstotzka.su>
2020-04-04 01:32:53 +0800

    Using ncov2019.live now for data. Simplified the code a bit too

--- TrumpBot/Modules/Commands/CoronaCommands.cs
+++ TrumpBot/Modules/Commands/CoronaCommands.cs
@@ -25,52 +25,9 @@ namespace TrumpBot.Modules.Commands
             public string HelpDescription { get; set; } = "Fetch worldwide Coronavirus stats";
             public List<string> RunCommand(ChannelMessageEventDataModel messageEvent, GroupCollection arguments = null, bool useCache = true)
             {
-                string pageHtml = Cache.Get<string>("Worldometers");
-                bool cached = true;
-                if (pageHtml == null)
-                {
-                    cached = false;
-                    Uri pageUri = new Uri("https://www.worldometers.info/coronavirus/");
-                    pageHtml = Http.Get(pageUri, fuzzUserAgent: true, compression: true, timeout: 20000);
-                    Cache.Set("Worldometers", pageHtml, DateTimeOffset.Now.AddMinutes(1));
-                }
-                HtmlDocument document = new HtmlDocument();
-                document.LoadHtml(pageHtml);
-                var nodes = document.DocumentNode.SelectNodes("//div[@class=\"maincounter-number\"]");
-                if (nodes.Count < 3)
-                {
-                    return new List<string>{"Page scraping is broken (nodes.Count < 3 for maincounter-number)"};
-                }
-
-                string cases = nodes[0]?.ChildNodes.First(c => c.Name == "span")?.InnerText?.Trim();
-                string deaths = nodes[1]?.ChildNodes.First(c => c.Name == "span")?.InnerText?.Trim();
-                string recovered = nodes[2]?.ChildNodes.First(c => c.Name == "span")?.InnerText?.Trim();
-
-                if (cases == null)
-                {
-                    return new List<string>{"Parsing of total case numbers is broken"};
-                }
-
-                if (deaths == null)
-                {
-                    return new List<string>{"Parsing of total deaths is broken"};
-                }
-
-                if (recovered == null)
-                {
-                    return new List<string>{"Parsing of recovered totals is broken"};
-                }
-
-                var lastUpdatedNode = document.DocumentNode.SelectSingleNode("//div[contains(text(), \"Last updated\")]");
-                var lastUpdatedSuccess = DateTime.TryParseExact(lastUpdatedNode.InnerText.Replace("Last updated: ", ""),
-                    "MMMM dd, yyyy, HH:mm GMT", null, DateTimeStyles.AssumeLocal, out var lastUpdated);
-                if (!lastUpdatedSuccess)
-                {
-                    return new List<string>{$"Cases: {cases}; Deaths: {deaths}; Recovered: {recovered}"};
-                }
-
-                var relativeTime = lastUpdated.Humanize(dateToCompareAgainst: DateTime.Now, utcDate: false);
-                return new List<string>{$"Cases: {cases}; Deaths: {deaths}; Recovered: {recovered}; Last Updated: {relativeTime}, Cached: {cached}"};
+                GroupCollection newCollection =
+                    new Regex(@"^coron\S+ (.+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase).Match("corona total").Groups;
+                return new GetCoronaCountryStats().RunCommand(messageEvent, newCollection, useCache);
             }
         }
 
@@ -88,46 +45,32 @@ namespace TrumpBot.Modules.Commands
             public List<string> RunCommand(ChannelMessageEventDataModel messageEvent, GroupCollection arguments = null, bool useCache = true)
             {
                 string country = arguments[1].Value.ToLower();
-                string pageHtml = Cache.Get<string>("Worldometers");
+                string pageHtml = Cache.Get<string>("Corona");
                 bool cached = true;
                 if (pageHtml == null)
                 {
                     cached = false;
-                    Uri pageUri = new Uri("https://www.worldometers.info/coronavirus/");
+                    Uri pageUri = new Uri("https://ncov2019.live/");
                     pageHtml = Http.Get(pageUri, fuzzUserAgent: true, compression: true, timeout: 20000);
-                    Cache.Set("Worldometers", pageHtml, DateTimeOffset.Now.AddMinutes(1));
+                    Cache.Set("Corona", pageHtml, DateTimeOffset.Now.AddMinutes(1));
                 }
                 HtmlDocument document = new HtmlDocument();
                 document.LoadHtml(pageHtml);
-                HtmlNode countryRow;
-                string countryFromTable;
-                // For rows with clickable links
-                var rowHref = document.DocumentNode.SelectSingleNode($"//a[translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')='{country}']");
-                if (rowHref == null)
+                var row = document.DocumentNode.SelectSingleNode($"//td[contains(translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz'),'{country}')]");
+                if (row == null)
                 {
-                    // For some reason there are spaces around the country
-                    var rowTd = document.DocumentNode.SelectSingleNode($"//td[translate(., 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')='{country}']");
-                    if (rowTd == null)
-                    {
-                        return new List<string>{"Can't find country in the table"};
-                    }
-
-                    countryFromTable = rowTd.InnerText.Trim();
-                    countryRow = rowTd.ParentNode;
+                    return new List<string>{"Can't find the country region, refer to the table at https://ncov2019.live/ to see acceptable names."};
                 }
-                else
-                {
-                    countryFromTable = rowHref.InnerText.Trim();
-                    countryRow = rowHref.ParentNode.ParentNode;
-                }
+                var countryFromTable = row.InnerText.Replace("&#9733;", string.Empty).Trim();
 
-                var columns = countryRow.ChildNodes.Where(c => c.Name == "td").ToList();
-                
+                var columns = row.ParentNode.ChildNodes.Where(c => c.Name == "td").ToList();
+
                 string cases = columns[1]?.InnerText?.Trim();
-                string deaths = columns[3]?.InnerText?.Trim();
-                string recovered = columns[5]?.InnerText?.Trim();
-                string active = columns[6]?.InnerText?.Trim();
-                string serious = columns[7]?.InnerText?.Trim();
+                var caseChange = columns[2]?.InnerText?.Trim() + $" ({columns[3]?.InnerText?.Trim()}%)";
+                string deaths = columns[4]?.InnerText?.Trim();
+                var deathChange = columns[5]?.InnerText?.Trim() + $" ({columns[6]?.InnerText?.Trim()}%)";
+                string recovered = columns[7]?.InnerText?.Trim();
+                string serious = columns[8]?.InnerText?.Trim();
                 if (cases == null || cases.Trim() == string.Empty)
                 {
                     cases = "None/Unknown";
@@ -140,25 +83,27 @@ namespace TrumpBot.Modules.Commands
                 {
                     recovered = "None/Unknown";
                 }
-                if (active == null || active.Trim() == string.Empty)
-                {
-                    active = "None/Unknown";
-                }
                 if (serious == null || serious.Trim() == string.Empty)
                 {
                     serious = "None/Unknown";
                 }
-                
-                var lastUpdatedNode = document.DocumentNode.SelectSingleNode("//div[contains(text(), \"Last updated\")]");
-                var lastUpdatedSuccess = DateTime.TryParseExact(lastUpdatedNode.InnerText.Replace("Last updated: ", ""),
-                    "MMMM dd, yyyy, HH:mm GMT", null, DateTimeStyles.AssumeLocal, out var lastUpdated);
-                if (!lastUpdatedSuccess)
+                if (caseChange == null || caseChange.Trim() == string.Empty)
                 {
-                    return new List<string>{$"Cases: {cases}; Deaths: {deaths}; Recovered: {recovered}"};
+                    caseChange = "None/Unknown";
                 }
-                var relativeTime = lastUpdated.Humanize(dateToCompareAgainst: DateTime.Now, utcDate: false);
+
+                if (deathChange == null || deathChange.Trim() == string.Empty)
+                {
+                    deathChange = "None/Unknown";
+                }
                 
-                return new List<string>{$"{countryFromTable}: Cases: {cases} (Active: {active}, Serious: {serious}, Deaths: {deaths}, Recovered: {recovered}); Last Updated: {relativeTime}, Cached: {cached}"};
+                var lastUpdatedNode = document.DocumentNode.SelectSingleNode("//p[contains(text(), \"updated:\")]//i");
+                if (lastUpdatedNode == null)
+                {
+                    return new List<string>{$"{countryFromTable}: Cases: {cases} (+{caseChange}), Serious: {serious}, Deaths: {deaths} (+{deathChange}), Recovered: {recovered}); Cached: {cached}"};
+                }
+                
+                return new List<string>{$"{countryFromTable}: Cases: {cases} (+{caseChange}), Serious: {serious}, Deaths: {deaths} (+{deathChange}), Recovered: {recovered}); Last Updated: {lastUpdatedNode.InnerText}, Cached: {cached}"};
             }
         }
     }
