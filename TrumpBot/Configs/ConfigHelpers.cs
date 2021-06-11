aa603bb48c2ed672132100cfdced95cd952e1d4f
BadAtIrcBots <BadAtIrcBots@arstotzka.su>
2021-06-12 01:02:40 +0800

    Updated ConfigPaths to use Path.Join() to fix Linux support

--- TrumpBot/Configs/ConfigHelpers.cs
+++ TrumpBot/Configs/ConfigHelpers.cs
@@ -17,18 +17,18 @@ namespace TrumpBot.Configs
 
         public static class ConfigPaths
         {
-            public static string CommandConfig { get; } = "Config\\command_config.json";
-            public static string CuckHuntConfig { get; } = "Config\\cuckhunt.json";
-            public static string AdminConfig { get; } = "Config\\admin_config.json";
-            public static string IrcConfig { get; } = "Config\\config.json";
-            public static string TwitterStreamConfig { get; } = "Config\\twitter_stream.json";
-            public static string WeatherApiConfig { get; } = "Config\\weather_api.json";
-            public static string RandomKickConfig { get; } = "Config\\random_kick.json";
-            public static string YouTubeApiConfig { get; } = "Config\\youtube_api.json";
-            public static string UrlHistoryConfig { get; } = "Config\\url_history.json";
-            public static string ChannelUriConfig { get; } = "Config\\uri.json";
-            public static string AirportConfig { get; } = "Config\\airports.json";
-            public static string IexApiConfig { get; } = "Config\\iex_api.json";
+            public static string CommandConfig => Path.Join("Config", "command_config.json");
+            public static string CuckHuntConfig => Path.Join("Config", "cuckhunt.json");
+            public static string AdminConfig => Path.Join("Config", "admin_config.json");
+            public static string IrcConfig => Path.Join("Config", "config.json");
+            public static string TwitterStreamConfig => Path.Join("Config", "twitter_stream.json");
+            public static string WeatherApiConfig => Path.Join("Config", "weather_api.json");
+            public static string RandomKickConfig => Path.Join("Config", "random_kick.json");
+            public static string YouTubeApiConfig => Path.Join("Config", "youtube_api.json");
+            public static string UrlHistoryConfig => Path.Join("Config", "url_history.json");
+            public static string ChannelUriConfig => Path.Join("Config", "uri.json");
+            public static string AirportConfig => Path.Join("Config", "airports.json");
+            public static string IexApiConfig => Path.Join("Config", "iex_api.json");
         }
     }
 }
